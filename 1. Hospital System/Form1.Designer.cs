ee4e6cbdb4e01f7181fe14e92b2ef3a61d7d0773
Ice713 <icramzilruiz@gmail.com>
2025-11-01 23:50:50 +0800

    Working with User Interface

--- 1. Hospital System/Form1.Designer.cs
+++ 1. Hospital System/Form1.Designer.cs
@@ -35,7 +35,7 @@
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
+            this.buttonDisplayPatient = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -97,21 +97,24 @@
             this.label4.TabIndex = 6;
             this.label4.Text = "Patient Details";
             // 
-            // button1
+            // buttonDisplayPatient
             // 
-            this.button1.Location = new System.Drawing.Point(330, 209);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 7;
-            this.button1.Text = "Display Patient Details";
-            this.button1.UseVisualStyleBackColor = true;
+            this.buttonDisplayPatient.BackColor = System.Drawing.Color.SpringGreen;
+            this.buttonDisplayPatient.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDisplayPatient.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.buttonDisplayPatient.Location = new System.Drawing.Point(306, 193);
+            this.buttonDisplayPatient.Name = "buttonDisplayPatient";
+            this.buttonDisplayPatient.Size = new System.Drawing.Size(141, 75);
+            this.buttonDisplayPatient.TabIndex = 7;
+            this.buttonDisplayPatient.Text = "Display Patient Details";
+            this.buttonDisplayPatient.UseVisualStyleBackColor = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.buttonDisplayPatient);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label3);
@@ -136,7 +139,7 @@
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonDisplayPatient;
     }
 }
 
