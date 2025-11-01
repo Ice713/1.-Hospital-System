ae94bd938b3f4270084c424aa8a66eb5bdfd86d1
Ice713 <icramzilruiz@gmail.com>
2025-11-01 23:53:11 +0800

    Done with Person Class, including getters, setters, and the virtual method for GetDetails()

--- 1. Hospital System/Person.cs
+++ 1. Hospital System/Person.cs
@@ -8,5 +8,17 @@ namespace _1.Hospital_System
 {
     internal class Person
     {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public virtual string GetDetails()
+        {
+            return $"Name: {Name}\n Age: {Age}";
+        }
     }
 }
